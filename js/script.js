d03ef948a570d56b911896b6db30d8ad56b2b9cd
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-08 16:39:59 +0100

    Edit JS script and fixed button @click events

--- js/script.js
+++ js/script.js
@@ -1,42 +1,36 @@
 // Dichiaro una Variabile contatore 
-let counter = 0;
-
 const {createApp } = Vue;
 
 createApp({
     data() {
         return {
+            currentSlide : 0,
             slides : [
                 {
                         image: 'img/01.webp',
                         title: 'Marvel\'s Spiderman Miles Morale',
                         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                         active : true,
-                        show: true
                     }, {
                         image: 'img/02.webp',
                         title: 'Ratchet & Clank: Rift Apart',
                         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                         active : false,
-                        show: false
                     }, {
                         image: 'img/03.webp',
                         title: 'Fortnite',
                         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                         active : false,
-                        show: false
                     }, {
                         image: 'img/04.webp',
                         title: 'Stray',
                         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                         active : false,
-                        show: false
                     }, {
                         image: 'img/05.webp',
                         title: "Marvel's Avengers",
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                         active : false,
-                        show: false
                     }
                 ]
         }
@@ -45,7 +39,7 @@ createApp({
 
         // Dichiaro una funzione che controlli l'evento del click sul bottone Avanti
         buttonForward() {
-
+            /*
             this.slides[counter].show = false;
             this.slides[counter].active = false;
 
@@ -58,22 +52,38 @@ createApp({
                 this.slides[counter].show = true;
                 this.slides[counter].active = true;
             }
-        
+            */
+            this.slides[this.currentSlide].active = false;
+
+            if (this.currentSlide < this.slides.length -1 ) {
+                this.currentSlide++;      
+  
+            } else {
+                this.currentSlide = 0;
+            }
+
+            this.slides[this.currentSlide].active = true;
+
         },
 
         buttonBack() {
-            this.slides[counter].show = false;
-            this.slides[counter].active = false;
+            this.slides[this.currentSlide].active = false;
 
-            if ((counter !== this.slides.length) && (counter !== 0)) {
-                counter --;
-                this.slides[counter].show = true;
-                this.slides[counter].active = true;
+            if ((this.currentSlide !== this.slides.length) && (this.currentSlide !== 0)) {
+                this.currentSlide --;
             } else { 
-               counter = this.slides.length -1;
-               this.slides[counter].show = true;
-               this.slides[counter].active = true;
+               this.currentSlide = this.slides.length -1;
             }
+
+            this.slides[this.currentSlide].active = true;
+        },
+
+        thumbShow(index) {
+            this.currentSlide.active = false;
+
+            this.currentSlide = index;
+
+            this.currentSlide.active = true;
         }
     }
 
